69d86b95e6c486fa010d08fba82b560f050893c1
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2020-08-02 17:36:09 -0300

    [ achieved background blur in iOS ]

--- app/core/transform/transform.cpp
+++ app/core/transform/transform.cpp
@@ -5,57 +5,58 @@
 
 
 cv::Mat Transformer::blur_background(cv::Mat image, cv::Mat mask) {
-  // resize mask
-  cv::resize(mask, mask, image.size());
-
-  cv::Mat result;
-  cv::Mat blured;
-
-  // convert all images into 32bit floats
-  result = cv::Mat(image.size(), CV_32FC3);
-  mask.convertTo(mask, CV_32FC3, 1.0/255.0);
-  image.convertTo(image, CV_32FC3, 1.0/255.0);
-
-  // blur both original image & mask (to get softer borders)
-  cv::blur(image, blured, cv::Size(91,91));
-  cv::blur(mask, mask, cv::Size(31,31));
-
-  cv::Mat M1, M2, M3;
-
-  // replace original image in blured one using given mask
-  cv::subtract(cv::Scalar::all(1.0), mask, M1);
-  cv::multiply(M1, image, M2);
-  cv::multiply(mask, blured, M3);
-  cv::add(M2, M3, result);
-
-  return result;
+    // resize mask
+    cv::resize(mask, mask, image.size());
+    
+    cv::Mat result;
+    cv::Mat blured;
+    
+    // convert all images into 32bit floats
+    result = cv::Mat(image.size(), CV_32FC3);
+    mask.convertTo(mask, CV_32FC3, 1.0/255.0);
+    image.convertTo(image, CV_32FC3, 1.0/255.0);
+    
+    // blur both original image & mask (to get softer borders)
+    cv::blur(image, blured, cv::Size(91,91));
+    cv::blur(mask, mask, cv::Size(31,31));
+    
+    cv::Mat M1, M2, M3;
+    
+    // replace original image in blured one using given mask
+    cv::subtract(cv::Scalar::all(1.0), mask, M1);
+    cv::multiply(M1, image, M2);
+    cv::multiply(mask, blured, M3);
+    cv::add(M2, M3, result);
+    
+    return result;
 }
 
 
 cv::Mat Transformer::replace_background(cv::Mat image, cv::Mat mask, cv::Mat background) {
-  // resize
-  cv::resize(image, image, size);
-  cv::resize(mask, mask, size);
-  cv::resize(background, background, size);
-
-  cv::Mat result;
-
-  // convert all images into 32bit floats
-  result = cv::Mat(image.size(), CV_32FC3);
-  mask.convertTo(mask, CV_32FC3, 1.0/255.0);
-  image.convertTo(image, CV_32FC3, 1.0/255.0);
-  background.convertTo(background, CV_32FC3, 1.0/255.0);
-
-  // blur mask (to get softer borders)
-  cv::blur(mask, mask, cv::Size(31,31));
-
-  cv::Mat M1, M2, M3;
-
-  // replace original image in background using given mask
-  cv::subtract(cv::Scalar::all(1.0), mask, M1);
-  cv::multiply(M1, image, M2);
-  cv::multiply(mask, background, M3);
-  cv::add(M2, M3, result);
-
-  return result;
+    // resize
+    cv::resize(image, image, size);
+    cv::resize(mask, mask, size);
+    cv::resize(background, background, size);
+    
+    cv::Mat result;
+    
+    // convert all images into 32bit floats
+    result = cv::Mat(image.size(), CV_32FC3);
+    mask.convertTo(mask, CV_32FC3, 1.0/255.0);
+    image.convertTo(image, CV_32FC3, 1.0/255.0);
+    background.convertTo(background, CV_32FC3, 1.0/255.0);
+    
+    // blur mask (to get softer borders)
+    cv::blur(mask, mask, cv::Size(31,31));
+    
+    cv::Mat M1, M2, M3;
+    
+    // replace original image in background using given mask
+    cv::subtract(cv::Scalar::all(1.0), mask, M1);
+    cv::multiply(M1, image, M2);
+    cv::multiply(mask, background, M3);
+    cv::add(M2, M3, result);
+    
+    result.convertTo(result, CV_8UC3, 255.0);
+    return result;
 }
